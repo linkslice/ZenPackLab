353f362abf4c986cc8ffe9a0ced86a757414a2a6
Bryan Irvine <bryan@frame.work>
2024-06-04 12:46:22 -0500

    adding upload script

--- upload.php
+++ upload.php
@@ -1,19 +1,30 @@
-
 <?php
 $target_dir = "libexec/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$scriptFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if(isset($_POST["submit"])) {
-  $check = getscriptsize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an script - " . $check["mime"] . ".";
-    $uploadOk = 1;
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
   } else {
-    echo "File is not an script.";
-    $uploadOk = 0;
+    echo "Sorry, there was an error uploading your file.";
   }
 }
 ?>
 
+go <a hreaf="/">back</a> to upload more files or create the pack
