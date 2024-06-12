ace1cf4671637ed2b59f7925ba0feca49ff5d606
Bryan Irvine <bryan@frame.work>
2024-06-12 11:58:16 -0500

    adding support for ubuntu images

--- zenpacklab.php
+++ zenpacklab.php
@@ -4,18 +4,23 @@ $author = $_GET["author"];
 $packname = $_GET["packname"];
 $version = $_GET["version"];
 
-if ( !is_null($_GET["nagios"]) )
-{
-    echo "Fetching Nagios plugins. Please be patient<br>";
-    $nagios_out = shell_exec('yum install -y nrpe nagios-plugins-http nagios-plugins-dig');
-}
-
 echo "Generating ZenPack<br>";
 chdir('CustomScriptBuilder');
-$makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version");
 
-echo "<a href='CustomScriptBuilder/$makepack_out'>$makepack_out</a>";
+if ( !is_null($_GET["nagios"]) )
+{
+    //makepack.sh should handle installations
+    //echo "Fetching Nagios plugins. Please be patient<br>";
+    //$nagios_out = shell_exec('yum install -y nrpe nagios-plugins-http nagios-plugins-dig');
+    $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version -p");
+}else{
+    $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version");
+}
 
+$eggs = glob("*.egg");
+foreach($eggs as $egg) {
+    echo "<a href='CustomScriptBuilder/$egg'>$egg</a><br>";
+}
 
 
 
