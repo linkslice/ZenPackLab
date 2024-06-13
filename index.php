900eece342ce6c8fffc75cee1b35297bd66d5665
Bryan Irvine <bryan@frame.work>
2024-06-13 10:57:09 -0500

    begrudging symlink support

--- index.php
+++ index.php
@@ -3,15 +3,24 @@
     Author:<input type=text name=author><br>
     Pack Name:<input type=text name=packname><br>
     Version: <input type=text name=version><br>
-    install nagios plugins?:<input type=checkbox name=nagios>
-    <input type=submit>
+    install nagios plugins?:<input type=checkbox name=nagios><br>
+    create symlinks during install?<input type=checkbox name=symlink> (don't don't this unless you know why)
+    <input type=submit value="create pack">
  </form>
 
-
+<hr>
 <form action="upload.php" method="post" enctype="multipart/form-data">
   Select scripts to upload:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="input script" name="submit">
 </form>
+<hr>
+
+<?php
+
+$packs = glob('*.egg');
+foreach($packs as $pack){
+  print "<a href='$pack'>$pack</a><br>"
+?>
 
 </html>
--- zenpacklab.php
+++ zenpacklab.php
@@ -9,12 +9,20 @@ chdir('CustomScriptBuilder');
 
 if ( !is_null($_GET["nagios"]) )
 {
-    //makepack.sh should handle installations
-    //echo "Fetching Nagios plugins. Please be patient<br>";
-    //$nagios_out = shell_exec('yum install -y nrpe nagios-plugins-http nagios-plugins-dig');
-    $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version -p");
+    if ( !is_null($_GET["symlink"]) )
+    {
+        $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version -p");
+    }else{
+        $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version -p -s");
+    }
+
 }else{
-    $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version");
+    if ( !is_null($_GET["symlink"]) )
+    {
+      $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version");
+    }else{
+      $makepack_out = shell_exec("/bin/bash makepack.sh -a '".$author."' -n $packname -v $version -s");
+    }
 }
 
 $eggs = glob("*.egg");
